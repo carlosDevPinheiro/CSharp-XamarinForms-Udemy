09aa6b86b8d6ebaf4de95e2b15fcab40ce29e9b4
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2017-12-05 19:47:54 -0200

    Commit inicial

---
+++ 01-Behavior_Triggers/App01/App01/App01/lib/EmailValidoTrigger.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace App01.lib
+{
+    public class EmailValidoTrigger: TriggerAction<Entry>
+    {
+
+        // metodo implementado 
+        protected override void Invoke(Entry sender)
+        {
+            //  se o texto do entry tiver o @ na posicao 0 em diante é valido
+            sender.TextColor = sender.Text.Contains("@") ? Color.Green : Color.Red ;
+        }
+    }
+}
---
+++ 01-Behavior_Triggers/App01/App01/App01/lib/MaiusculoBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+
+namespace App01.lib
+{
+    public class MaiusculoBehavior: Behavior<Entry>  // Behavior => Comportamento
+    {
+        // adiciona uma informção ao campo entry
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+
+        // remove a informação
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+        
+        private static void MaiusculoAction(object sender, TextChangedEventArgs args)
+        {
+            /* 
+             *  
+             *  prop NewTextValue - novo valor do campo
+             *  prop  OldTextValue - valor anterior 
+             */
+
+            // verifica se o entry contem algum valor
+            if (!string.IsNullOrEmpty(args.OldTextValue))
+            {
+                ((Entry) sender).Text = args.NewTextValue.ToUpper();
+            }
+        }
+
+       
+    }
+}
+
+
+
