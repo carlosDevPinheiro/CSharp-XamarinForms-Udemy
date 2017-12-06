e92cc906b1c06fc9a7a3fc9f6aaaaf8bd512dec9
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2017-12-06 11:40:38 -0200

    Aula 2 concluida listviw

---
+++ 02-ListView/App02/App02/App02/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace App02
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ListaFuncionarios.ItemsSource = GetFuncionario();
+        }
+
+
+        public List<Grupo> GetFuncionario()
+        {
+            return new List<Grupo>
+            {
+                new Grupo("Presidente","CEO","Presidente da America")
+                {
+                    new Pessoa() { Nome = "Jose", IsObrigatorio = true, RankEficiencia = 8}
+                },
+                new Grupo("Diretores","Dir.", "America do Sul")
+                {
+                    new Pessoa(){ Nome = "Maria", IsObrigatorio = true, RankEficiencia = 8},
+                    new Pessoa(){ Nome = "João", IsObrigatorio = false}
+                },
+                new Grupo("Gerentes","Ger.", "Brasil")
+                {
+                    new Pessoa(){ Nome = "Felipe", IsObrigatorio = true, RankEficiencia = 8},
+                    new Pessoa(){ Nome = "Judas", IsObrigatorio = false}
+                },
+                new Grupo("Funcionarios","Ope", "Funcionarios da empresa")
+                {
+                        new Pessoa { Nome = "Felipe - Funcionario",IsObrigatorio = false,},
+                        new Pessoa { Nome = "Judas - Funcionario",IsObrigatorio = false, },
+                        new Pessoa { Nome = "Jose - Funcionario",IsObrigatorio = false,  },
+                        new Pessoa { Nome = "Maria - Funcionario",IsObrigatorio = false,},
+                        new Pessoa { Nome = "João - Funcionario",IsObrigatorio = true, RankEficiencia = 6},
+                        new Pessoa { Nome = "Felipe - Funcionario",IsObrigatorio = false},
+                        new Pessoa { Nome = "Judas - Funcionario",IsObrigatorio = false},
+                        new Pessoa { Nome = "Danilo - Funcionario",IsObrigatorio = false},
+                        new Pessoa { Nome = "Helen - Funcionario",IsObrigatorio = false}
+                }
+            };
+        }
+
+        public class Grupo : List<Pessoa>
+        {
+            public Grupo(string titulo, string tituloCurto, string descricao)
+            {
+                Titulo = titulo;
+                TituloCurto = tituloCurto;
+                Descricao = descricao;
+            }
+
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
+
+        }
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public int RankEficiencia { get; set; }
+            public bool IsObrigatorio { get; set; }
+        }
+    }
+
+
+
+}
---
+++ 02-ListView/App02/App02/App02/TemplateCustomizado/SeletorTemplateCarlos.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using Xamarin.Forms;
+
+namespace App02.TemplateCustomizado
+{
+    public class SeletorTemplateCarlos: DataTemplateSelector
+    {
+        public DataTemplate ItemPessoaObrigatoria { get; }
+        public DataTemplate ItemPessoaNObrigatoria { get; }
+
+        public SeletorTemplateCarlos()
+        {
+            ItemPessoaObrigatoria = new DataTemplate(typeof(ItemPessoaObrigatorioViewCell));
+            ItemPessoaNObrigatoria = new DataTemplate(typeof(ItemPessoaNObrigatorioViewCell));
+        }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((MainPage.Pessoa) item).IsObrigatorio ? ItemPessoaObrigatoria : ItemPessoaNObrigatoria;
+        }
+    }
+}
